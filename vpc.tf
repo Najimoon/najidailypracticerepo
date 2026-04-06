f08b290767c331070adf35837034e4f2b3c26c0e
najimoon <sknajimoon143@gmail.com>
2026-04-06 12:09:42 +0530

    dficure

--- vpc.tf
+++ vpc.tf
@@ -7,17 +7,15 @@ resource"aws_vpc" "name" {
 
 resource"aws_vpc" "name" {
 
-    tags ={
+    tags =
         name = "najimoon-vpc02"
 <<<<<<< HEAD
         owner ="prodvpc"
         department ="it"
-=======
+
         owner ="devvpc"
-<<<<<<< HEAD
-        project ="gutyj"
-=======
->>>>>>> 876848340c28e2db5d6d98fdca63cb31dc83ad46
->>>>>>> 00247411b23a32b9d1b880643c8cf640b072d45b
+       project ="gutyj"{
+
+
     }
 }
